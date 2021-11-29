9fa4809b851b15ceea026a39f01beaa9fb4b89c6
hakim <kimoaouini20@gmail.com>
2021-11-29 10:40:08 +0100

    Store_data_in_ViewModel

---
+++ app/src/main/java/com/hakimaouini/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,13 @@
+package com.hakimaouini.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+
+
+    class GameViewModel : ViewModel() {
+
+        private var score = 0
+        private var currentWordCount = 0
+        private var currentScrambledWord = "test"
+
+
+    }
